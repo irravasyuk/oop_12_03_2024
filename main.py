8bbd726125db813540c8da6fc9244c34d0fb5b7f
irravasyuk <vasukirina29@gmail.com>
2024-03-12 20:19:37 +0200

    клас Circle

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
-# Завдання 1
-# Створіть клас Student з атрибутами name та age.
-# Додайте метод print_info, який виведе інформацію про
-# студента у на вигляді "Ім'я: {name}, Вік: {age}".
+# Завдання 2
+#  Створіть клас Circle з атрибутом radius та методом
+# area, який поверне площу кола з вказаним радіусом.
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
 
+    def area(self):
+        print(f"Площа кола - {3.14 * self.radius**2}")
 
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+circle = Circle(3)
+circle.area()
 
-    def print_info(self):
-        print(f"Ім'я студента - {self.name} та його вік {self.age}")
-
-student1 = Student("Іван", 20)
-student1.print_info()
-
-# class Dogs():
-#     def __init__(self, name, age, breed):
-#         self.name = name
-#         self.age = age
-#         self.breed = breed
-#
-#
-# my_dog = Dogs("Бобік", 5, "Лабродор")
-#
-# print(my_dog.name)
-# print(my_dog.age)
 
